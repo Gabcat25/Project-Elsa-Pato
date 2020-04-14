69e0b437a7cda3a852ae5c8f22d732fb7d63b1c7
gabri <gabri@LAPTOP-62IBIE85>
2020-04-14 17:59:20 +0200

    Que Fou

---
+++ src/repositori/poo/projecte/RepositoriPOOProjecte.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositori.poo.projecte;
+
+/**
+ *
+ * @author gabri
+ */
+public class RepositoriPOOProjecte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
